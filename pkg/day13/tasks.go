2c74c73a75626b70b9518d376efa4801b8f40d07
Ales Bolka <ales.bolka@paradoxinteractive.com>
2022-12-19 09:05:55 +0100

    day13 task 2

--- pkg/day13/tasks.go
+++ pkg/day13/tasks.go
@@ -1,6 +1,7 @@
 package day13
 
 import (
+	"log"
 	"strings"
 )
 
@@ -26,5 +27,39 @@ func Task1(input string) int {
 }
 
 func Task2(input string) int {
-	return 0
+	lines := strings.Split(input, "\n")
+	pack1 := NewPacket("[[2]]")
+	pack2 := NewPacket("[[6]]")
+	packets := []*Packet{}
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		packets = append(packets, NewPacket(line))
+	}
+
+	pos1 := 1
+	pos2 := 1
+
+	for ii := 0; ii < len(packets); ii++ {
+		if pack1.Compare(packets[ii]) == -1 {
+			pos1++
+		}
+		if pack2.Compare(packets[ii]) == -1 {
+			pos2++
+		}
+	}
+
+	switch pack1.Compare(pack2) {
+	case 0:
+		log.Fatalf("did not expect divider packages to be unsortable")
+	case 1:
+		pos2++
+	case -1:
+		pos1++
+	}
+
+	return pos1 * pos2
 }
